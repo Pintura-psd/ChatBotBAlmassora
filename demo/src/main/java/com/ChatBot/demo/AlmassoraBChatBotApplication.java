1d67747bb16a70d788eec83646ce745933488a94
Pintura-psd <usuhy1999@gmail.com>
2025-11-27 12:49:42 +0100

    Proyecto empezdo

---
+++ demo/src/main/java/com/ChatBot/demo/AlmassoraBChatBotApplication.java
@@ -0,0 +1,13 @@
+package com.ChatBot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlmassoraBChatBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlmassoraBChatBotApplication.class, args);
+	}
+
+}
