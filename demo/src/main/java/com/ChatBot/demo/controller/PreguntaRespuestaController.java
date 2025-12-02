577fcb03505fb5c129fa0389bbabb23e88f0b14d
hector <hencinas23@gmail.com>
2025-12-02 10:23:38 +0100

    hecho cosas

--- demo/src/main/java/com/ChatBot/demo/controller/PreguntaRespuestaController.java
+++ demo/src/main/java/com/ChatBot/demo/controller/PreguntaRespuestaController.java
@@ -1,4 +1,37 @@
 package com.ChatBot.demo.controller;
 
+import com.ChatBot.demo.model.PreguntaRespuesta;
+import com.ChatBot.demo.service.PreguntaRespuestaService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/preguntarespuesta")
 public class PreguntaRespuestaController {
+    private final PreguntaRespuestaService pregResp;
+
+    public PreguntaRespuestaController(PreguntaRespuestaService pregResp){
+        this.pregResp = pregResp;
+    }
+    //crear pregunta respuesta
+    @PostMapping
+    public PreguntaRespuesta crear(@RequestBody PreguntaRespuesta preg){
+        return pregResp.crearPreguntaRespuesta(preg);
+    }
+    //obtener todas lasw preguntas
+    @GetMapping
+    public List<PreguntaRespuesta> obtenerPreguntas(){
+        return pregResp.getPreguntaRespuestas();
+    }
+    //actualizar pregunta respuesta
+    @PutMapping("/{id}")
+    public PreguntaRespuesta update(@PathVariable String id, @RequestBody PreguntaRespuesta preg){
+        return pregResp.updatePreguntaRespuesta(id,preg);
+    }
+    //Borrar pregunta respuesta
+    @DeleteMapping("/{id}")
+    public void eliminarPreguntaRespuesta(@PathVariable String id){
+        pregResp.deletePreguntaRespuesta(id);
+    }
 }
--- demo/src/main/java/com/ChatBot/demo/service/PreguntaRespuestaService.java
+++ demo/src/main/java/com/ChatBot/demo/service/PreguntaRespuestaService.java
@@ -4,6 +4,8 @@ import com.ChatBot.demo.model.PreguntaRespuesta;
 import com.ChatBot.demo.repository.PreguntaRespuestaRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class PreguntaRespuestaService {
     private final PreguntaRespuestaRepository preguntaRespuestaRepository;
@@ -12,4 +14,20 @@ public class PreguntaRespuestaService {
 
     }
 
+    //Crear el objeto pregunta respuesta
+    public PreguntaRespuesta crearPreguntaRespuesta(PreguntaRespuesta preguntaRespuesta){
+        return preguntaRespuestaRepository.save(preguntaRespuesta);
+    }
+
+    public List<PreguntaRespuesta> getPreguntaRespuestas(){
+        return preguntaRespuestaRepository.findAll();
+    }
+
+    public PreguntaRespuesta updatePreguntaRespuesta(String id,PreguntaRespuesta pre) {
+      pre.setId(id);
+      return preguntaRespuestaRepository.save(pre);
+    }
+    public void deletePreguntaRespuesta(String id){
+        preguntaRespuestaRepository.deleteById(id);
+    }
 }
