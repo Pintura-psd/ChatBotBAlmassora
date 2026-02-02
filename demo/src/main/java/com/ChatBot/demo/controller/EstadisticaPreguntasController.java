541d6bc1eb1e1d46313829367d22cf47d99f456d
hector <hencinas23@gmail.com>
2026-02-02 10:35:18 +0100

    Estadisticas

--- demo/src/main/java/com/ChatBot/demo/controller/EstadisticaPreguntasController.java
+++ demo/src/main/java/com/ChatBot/demo/controller/EstadisticaPreguntasController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -16,6 +17,23 @@ public class EstadisticaPreguntasController {
 
     public EstadisticaPreguntasController(EstadisticasService service) {
         this.service = service;
+    }
+
+    @GetMapping("/preguntasfrecuentes")
+    public List<PreguntaFrecuenciaDTO> getPreguntasFrecuentes(){
+        return service.top5UltimaHora();
+    }
+    @GetMapping("/totalpreguntas")
+    public int totalPreguntas(){
+        return service.totalPreguntas();
+    }
+    @GetMapping("/preguntasSinRespuesta")
+    public int preguntasSinRespuesta(){
+        return service.preguntasSinRespuesta();
+    }
+    @GetMapping("/preguntasConRespuesta")
+    public int preguntasConRespuesta(){
+        return service.preguntasConRespuesta();
     }
 
 
