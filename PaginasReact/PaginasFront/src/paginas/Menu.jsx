c849f3af8f631715be3681553457ab5185849b6d
paul200234 <paul200234@gmail.com>
2026-02-24 13:05:00 +0100

    card en menu para edit

--- PaginasReact/PaginasFront/src/paginas/Menu.jsx
+++ PaginasReact/PaginasFront/src/paginas/Menu.jsx
@@ -128,6 +128,43 @@ const DashboardCards = () => {
                                 </Card.Footer>
                             </Card>
                         </Col>
+
+                        {/* Editar Preguntas */}
+                        <Col md={4} className="mb-4 d-flex justify-content-center">
+                            <Card
+                                className="mb-3 border border-dark p-0 rounded-5"
+                                style={{ borderRadius: "5px", width: "400px" }}
+                            >
+                                <Card.Header className="bg-dark text-white fw-semibold px-4 py-3 border-0 rounded-top-5 text-center">
+                                    <h3>Editar Preguntas</h3>
+                                </Card.Header>
+
+                                <Card.Body className="p-4 text-center">
+                                    <div
+                                        className="d-inline-flex align-items-center justify-content-center bg-success rounded-circle mb-3"
+                                        style={{ width: "80px", height: "80px" }}
+                                    >
+                                        <i className="bi bi-pencil-square fs-1 text-white"></i>
+                                    </div>
+
+                                    <Card.Text className="text-dark opacity-75">
+                                        Edita, modifica y actualiza las preguntas
+                                        existentes en la base de conocimiento del ChatBot.
+                                    </Card.Text>
+
+                                </Card.Body>
+                                <Card.Footer className="d-flex justify-content-center border-0">
+                                    <Button
+                                    as={Link} 
+                                    to="/edit"
+                                    variant="outline-dark" 
+                                    className="rounded-pill px-4"
+                                    >
+                                        Editar Preguntas
+                                    </Button>
+                                </Card.Footer>
+                            </Card>
+                        </Col>
                     </Row>
 
 
